5a6921a5021b965c76dc00ef4aaa25cbc4e2d93b
Chris Padilla <cdpadilla42@gmail.com>
2021-07-19 11:29:14 -0500

    Correct endpoint name

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const devEndpoint = `http://localhost:3000/api`;
-export const prodEndpoint = `https://sxsw-house-contert-next-cut0ehhqr-cdpadilla42.vercel.app/api`;
+export const prodEndpoint = `https://sxsw-house-contert-next.vercel.app/api`;
 export const endpoint =
   process.env.NODE_ENV === 'development' ? devEndpoint : prodEndpoint;
