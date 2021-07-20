eeafd0a181f41641c4cd1234640ccaf5e2578d8c
Chris Padilla <cdpadilla42@gmail.com>
2021-07-20 10:52:08 -0500

    Bug fix

--- pages/api/restaurants.js
+++ pages/api/restaurants.js
@@ -48,7 +48,9 @@ export default async function handler(req, res) {
 
       const { data, totalCount } = results[0];
 
-      res.status(200).json({ success: true, data, totalCount: totalCount[0] });
+      res
+        .status(200)
+        .json({ success: true, data, totalCount: totalCount[0].count });
       break;
     case 'POST':
       const newRestaurant = await Restaurants.create({
